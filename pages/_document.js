87cd2fcec226fad01f89ad29b96d0fd836208947
Adriel Machado <adri@newtonhewitt.com>
2023-06-20 08:40:24 -0300

    Questrial sin document

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
             href="https://fonts.googleapis.com/css2?family=Questrial&display=swap"
             rel="stylesheet"
         />
       </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
     </Html>
   )
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      <link
+          href="https://fonts.googleapis.com/css2?family=Questrial&display=swap"
+          rel="stylesheet"
+      />
       <body>
         <Header />
         {children}
